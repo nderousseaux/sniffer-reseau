c9feb7e63bb49f383a3f7759bb0fefb2cb0cbeb8
Nathanaël Derousseaux <n.derousseaux@icloud.com>
2022-09-29 13:55:16 +0200

    BUILD: Structure de base

---
+++ src/includes/devices.h
@@ -0,0 +1,10 @@
+#ifndef H_GL_HELLO
+#define H_GL_HELLO
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pcap.h>
+
+void find_device(void);
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "includes/devices.h"
+
+int main(void)
+{
+    find_device();
+    return EXIT_SUCCESS;
+}
